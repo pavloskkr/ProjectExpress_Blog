7d487363196d0fd595f6a4f227744b99001d8dfd
pkkr <p.kakarantzas@hypertech.gr>
2024-08-28 17:19:31 +0300

    implemented a logger and a jwt auth

--- src/interface/routes/ArticleRoutes.ts
+++ src/interface/routes/ArticleRoutes.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import {createArticle, deleteArticle, getArticle, getArticles, updateArticle} from "../controllers/ArticleController";
+import {authenticateToken} from "../middleware/auth";
 
 const router = Router();
 
-router.get('/articles', getArticles);
+router.get('/articles', authenticateToken, getArticles);
 router.get('/articles/:id', getArticle);
 router.post('/articles', createArticle);
 router.delete('/articles/:id', deleteArticle);
