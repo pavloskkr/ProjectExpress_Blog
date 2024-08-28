fd8843ca51efa61d48e08bb64ee64c0c6120a2d3
pkkr <p.kakarantzas@hypertech.gr>
2024-08-28 16:11:31 +0300

    Added Dependency Injection Container Approach

--- src/interface/controllers/ArticleController.ts
+++ src/interface/controllers/ArticleController.ts
@@ -1,16 +1,9 @@
 import {Request, Response} from "express";
-import {RedisArticleRepository} from "../../infrastructure/repositories/RedisArticleRepository";
-import {GetAllArticles} from "../../use-cases/GetAllArticles";
 import {Article} from "../../domain/entities/Article";
-import {CreateArticle} from "../../use-cases/CreateArticle";
-import {UpdateArticle} from "../../use-cases/UpdateArticle";
-import {DeleteArticle} from "../../use-cases/DeleteArticle";
-import {GetArticleById} from "../../use-cases/GetArticleById";
-
-const articleRepository = new RedisArticleRepository();
+import {DIContainer} from "../../infrastructure/DIContainer";
 
 export const getArticles = async (req: Request, res: Response): Promise<void> => {
-    const getAllArticles = new GetAllArticles(articleRepository);
+    const getAllArticles = DIContainer.getGetAllArticlesUseCase();
     const articles = await getAllArticles.execute();
     res.json(articles);
 }
@@ -18,7 +11,7 @@ export const getArticles = async (req: Request, res: Response): Promise<void> =>
 export const getArticle = async (req: Request, res: Response): Promise<void> => {
     const {id} = req.params
 
-    const getArticleById = new GetArticleById(articleRepository);
+    const getArticleById = DIContainer.getGetArticleByIdUseCase()
     const article = await getArticleById.execute(id);
     res.json(article);
 }
@@ -27,7 +20,7 @@ export const createArticle = async (req: Request, res: Response): Promise<void> 
     const {id, title, content, author} = req.body
     const article = new Article(id, title, content, author, new Date());
 
-    const createArticleUseCase = new CreateArticle(articleRepository);
+    const createArticleUseCase = DIContainer.getCreateArticleUseCase();
     await createArticleUseCase.execute(article);
 
     res.status(201).json({ message: 'Article created successfully ' });
@@ -37,7 +30,7 @@ export const updateArticle = async (req: Request, res: Response): Promise<void> 
     const {id, title, content, author} = req.body
     const article = new Article(id, title, content, author, new Date());
 
-    const updateArticleUseCase = new UpdateArticle(articleRepository);
+    const updateArticleUseCase = DIContainer.getUpdateArticleUseCase();
     await updateArticleUseCase.execute(article);
     res.status(201).json({ message: 'Article updated successfully ' });
 }
@@ -45,7 +38,7 @@ export const updateArticle = async (req: Request, res: Response): Promise<void> 
 export const deleteArticle = async (req: Request, res: Response): Promise<void> => {
     const {id} = req.params
 
-    const deleteArticleUseCase = new DeleteArticle(articleRepository);
+    const deleteArticleUseCase = DIContainer.getDeleteArticleUseCase();
     await deleteArticleUseCase.execute(id);
 
     res.status(201).json({ message: 'Article deleted successfully ' });
